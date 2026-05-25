cb04c937cdde8274edf6ea0694c739523a834fb2
delaire thibault <thibault_d@outlook.fr>
2026-05-25 17:14:49 +0200

    agevalidation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :cocktails
   has_many :chats
+  validate over18: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
