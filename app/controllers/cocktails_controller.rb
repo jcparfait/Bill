480d4f38ac9800d760e7507bff8521330acb36eb
dina ramaroson <dina.ramaroson@fuwear-technology.com>
2026-05-26 13:00:14 +0200

    controller

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,56 @@
+class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [ :show, :edit, :update, :destroy ]
+
+  def index
+    @cocktails = current_user.cocktails
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.user = current_user
+
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @cocktail.update(cocktail_params)
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path, status: :see_other
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(
+      :name,
+      :ingredients,
+      :recipe,
+      :mood
+    )
+  end
+end
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,3 +1,8 @@
 class Cocktail < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :ingredients, presence: true
+  validates :recipe, presence: true
+  validates :mood, presence: true
 end
