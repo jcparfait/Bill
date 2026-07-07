2e7aba06271e865023c47459b508013f9eba7397
jeanchristophe parfait <jeanchristopheparfait@gmail.com>
2026-07-07 08:48:49 +0200

    favicon

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_29_102850) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_06_123000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -32,6 +32,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_29_102850) do
     t.string "mood"
     t.string "name"
     t.text "recipe"
+    t.boolean "saved", default: true, null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id", "external_id"], name: "index_cocktails_on_user_id_and_external_id_unique", unique: true, where: "(external_id IS NOT NULL)"
